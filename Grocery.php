685280b52a57ae2fed49683cd9eb16af31dfb589
starmangk <starmangk@gmail.com>
2016-04-29 13:32:05 +0200

    Nu finns en style inlagd

--- Grocery.php
+++ Grocery.php
@@ -1,9 +1,17 @@
 <?php
-
+echo '<html>
+echo '<head>
+echo '	<style>
+		.thecontent{
+			background-color:#ffffff
+		}
+	</style>'
+echo '</head>
+echo '<body>'
 echo '<br>';
 echo '<div>';
 
+	
 echo '</div>';
-
-
+echo '</body>';
 ?>
