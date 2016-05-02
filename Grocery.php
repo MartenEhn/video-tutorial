f9e7f6473ae176614623c21fca8ea6fe85c6cc57
starmangk <starmangk@gmail.com>
2016-05-02 13:33:30 +0200

    Feature 6

--- Grocery.php
+++ Grocery.php
@@ -12,7 +12,6 @@ echo '<br>';
 echo '<div class="thecontent">';
 		Hello World! This is a complete useless Grocery.	
 		However, tested in a branch.
-		feature 6
 		
 echo '</div>';
 echo '</body>';
