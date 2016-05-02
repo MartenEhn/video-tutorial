f523c11ecb83533dbc5b0ee6467140705518f66d
starmangk <starmangk@gmail.com>
2016-05-02 11:30:25 +0200

    Finish a feature

--- Grocery.php
+++ Grocery.php
@@ -12,7 +12,9 @@ echo '<br>';
 echo '<div class="thecontent">';
 		Hello World! This is a complete useless Grocery.	
 		However, tested in a branch.
-
+		An new feature v-t-f2
+		An new feature v-t-f2 b
+		
 echo '</div>';
 echo '</body>';
 ?>
